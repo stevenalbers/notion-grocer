28260722c1a5b4e4f800366cfad9dd585d06dc41
Steven Albers <stevenalbers@gmail.com>
2023-02-28 22:14:31 -0800

    feat: implement basic stock manager

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,76 @@
+import { BaseSyntheticEvent, useState } from "react";
+
+interface Item {
+  name: string;
+  amount: "Out" | "Low" | "Stocked";
+}
+
 export function App() {
-    return <h1>Hello world!</h1>;
-  }
+  const defaultList: Item[] = [
+    { name: "Eggs", amount: "Stocked" },
+    { name: "Milk", amount: "Stocked" },
+    { name: "Butter", amount: "Stocked" },
+  ];
+  const [items, setItems] = useState<Item[]>(defaultList);
+  const [currentItem, setCurrentItem] = useState(items[0]);
+
+  const handleStockClick = (e: BaseSyntheticEvent) => {
+    const updatedItems = items.map((item) => {
+      if (item.name === currentItem.name) {
+        return { name: currentItem.name, amount: e.target.id };
+      }
+      return item;
+    });
+
+    setItems(updatedItems);
+
+    if (updatedItems[updatedItems.length - 1]?.name === currentItem.name) {
+      setCurrentItem(null);
+    } else {
+      setCurrentItem(items[items.findIndex((item) => item.name === currentItem.name) + 1]);
+    }
+  };
+
+  const renderShoppingList = () => {
+    const shoppingList = items
+      .filter((item) => item.amount !== "Stocked")
+      .map((item) => (
+        <li key={item.name}>
+          {item.name}: {item.amount}
+        </li>
+      ));
+
+    return shoppingList.length > 0 ? (
+      <>
+        <h1>Here's our shopping list:</h1>
+        {shoppingList}
+      </>
+    ) : (
+      <span>All stocked!</span>
+    );
+  };
+
+  return (
+    <div className="h-full bg-gray-100 flex flex-col items-center justify-center">
+      {currentItem && (
+        <>
+          <h1>How stocked are we on</h1>
+          <h2>{currentItem?.name}</h2>
+          <div className="">
+            <button onClick={handleStockClick} className="btn btn-red" id="Out">
+              Out
+            </button>
+            <button onClick={handleStockClick} className="btn btn-yellow" id="Low">
+              Low
+            </button>
+            <button onClick={handleStockClick} className="btn btn-green" id="Stocked">
+              Stocked
+            </button>
+          </div>
+        </>
+      )}
+
+      {!currentItem && <div>{renderShoppingList()}</div>}
+    </div>
+  );
+}
